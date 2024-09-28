d221a50f5aa96616bf7dab036d1c269da8cc5ab7
tonydevpro <antonio.1358@hotmail.com>
2024-09-28 09:59:37 -0300

    finalizar

--- javascript.js
+++ javascript.js
@@ -24,3 +24,25 @@ let menu = document.getElementById('menu').addEventListener('click', ()=>{
     let caixaMenu = document.getElementById('caixa-menu');
     caixaMenu.style.display = (caixaMenu.style.display === 'none' || caixaMenu.style.display === '') ? 'flex' : 'none';
 })
+
+let adicionar = document.getElementById('addCar').addEventListener('click', ()=>{
+    let caixaMenu = document.getElementById('carrinho-menu');
+    
+
+    caixaMenu.innerHTML = `<div id="carrinho-cheio">
+    <div id="carrinho-pedido">
+        <img src="./img/image-product-1-thumbnail.jpg" alt="imagem de um tenis">
+        <P>Tenis edição limitada de outono <br> $125,00 x ${numeroItens} <span><b>$${125*numeroItens}</b></span></P>
+        <button id="excluir" ><i class="fa-solid fa-square-xmark"></i></button>
+    </div>
+    <button id="comprar" ><b>Comprar</b></button>
+</div>`;
+
+let excluir = document.getElementById('excluir').addEventListener('click', ()=>{
+    
+    caixaMenu.innerHTML =`<div id="carrinho-menu"> 
+            <h3>seu carrinho está vazio.</h3>
+        </div>`
+    })
+})
+
